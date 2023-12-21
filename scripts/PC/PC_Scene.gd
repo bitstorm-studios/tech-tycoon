0c4f10af54ac98ad917cd129b90141dc6034cf94
Fabio de Araujo <fabioarujo1608@gmail.com>
2023-12-21 14:47:04 -0300

    Alpha 1.0

--- scripts/PC/PC_Scene.gd
+++ scripts/PC/PC_Scene.gd
@@ -3,14 +3,11 @@ extends Control
 var catch_game_scene:PackedScene = preload("res://scenes/Catch Game/Background.tscn")
 var space_shooter_scene:PackedScene = preload("res://scenes/Space Shooter/game.tscn")
 var finish_scene:PackedScene = preload("res://scenes/PC/Conclude.tscn")
-var pendent_tasks = 0
+var pendent_tasks = 2
 
 func _on_tampa_button_pressed():
 	get_node("Panel/PC/Tampa").queue_free()
-	if pendent_tasks == 0:
-		var finish_intance = finish_scene.instantiate()
-		self.add_child(finish_intance)
-
+	
 func _on_ram_button_pressed():
 	var space_shooter_scene_instantiate = space_shooter_scene.instantiate()
 	get_tree().get_root().get_node("/root/Game").add_child(space_shooter_scene_instantiate)
