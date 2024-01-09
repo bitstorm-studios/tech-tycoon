5ab42565917b6e11a1df21d8bfbf200407959e0e
rafaelnmcosta <rafaelnmcosta@outlook.com>
2024-01-09 19:15:00 -0300

    trocando hud

--- scripts/PC/PC_Scene.gd
+++ scripts/PC/PC_Scene.gd
@@ -3,6 +3,8 @@ extends Control
 var catch_game_scene:PackedScene = preload("res://scenes/Catch Game/Background.tscn")
 var space_shooter_scene:PackedScene = preload("res://scenes/Space Shooter/game.tscn")
 var finish_scene:PackedScene = preload("res://scenes/PC/Conclude.tscn")
+var task_complete = preload("res://assets/PC/taskcompleted-1.png.png")
+
 var pendent_tasks = 2
 
 func _on_tampa_button_pressed():
@@ -22,23 +24,25 @@ func check_finish():
 	pendent_tasks -= 1
 	if pendent_tasks == 0:
 		var finish_intance = finish_scene.instantiate()
-		get_node("Panel/Mesa/Tasks_panel/Finish_container").add_child(finish_intance)
+		get_node("Panel/Mesa/Tasks_panel/Tasks_container/Finish_container").add_child(finish_intance)
 
 func _on_cpu_button_pressed():
 	get_node("Panel/PC/CPU").hide()
 	get_node("Panel/PC/Button_container/CPU_button").queue_free()
-	lauch_space_shooter()
 	check_finish()
 
 func _on_hd_button_pressed():
 	get_node("Panel/PC/HD").hide()
 	get_node("Panel/PC/Button_container/HD_button").queue_free()
+	get_node("Panel/Mesa/Tasks_panel/Tasks_container/Task1_container/Check_1").set_texture(task_complete)
 	launch_catch_game()
 	check_finish()
 
 func _on_ram_button_pressed():
 	get_node("Panel/PC/RAM").hide()
 	get_node("Panel/PC/Button_container/RAM_button").queue_free()
+	lauch_space_shooter()
+	get_node("Panel/Mesa/Tasks_panel/Tasks_container/Task2_container/Check_2").set_texture(task_complete)
 	check_finish()
 
 func _on_gpu_button_pressed():
--- scripts/Space Shooter/GameOverUI.gd
+++ scripts/Space Shooter/GameOverUI.gd
@@ -5,5 +5,5 @@ var space_scene = preload("res://scenes/Space Shooter/game.tscn")
 func _on_button_pressed():
 	get_tree().paused = false
 	var space_instance = space_scene.instantiate()
-	get_parent().queue_free()
+	get_parent().get_parent().get_parent().queue_free()
 	get_tree().get_root().get_node("/root/Game").add_child(space_instance)
